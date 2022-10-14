b88554052cbbc3041fe8dcc02a5ed6d5ac818126
vasilina <vasilinastulova@gmail.com>
2022-10-15 00:51:38 +0300

    first

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,13 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const num = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    const digits = Object.keys(num);
+    for(let i=0; i<str.length; i++){
+        if (digits.indexOf(str[i]) < digits.indexOf(str[i+1])) {
+            result -= num[str[i]];
+        } else {
+            result += num[str[i]];
+        }
+    }
     return result;
 }
